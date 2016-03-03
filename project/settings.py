0bf5be089784220b8d02d7534e6ea8a993783c01
Inaki Cabo <icaboalo@gmail.com>
2016-03-03 07:24:52 -0600

    postgres

--- project/settings.py
+++ project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-}
+# }
 
 DATABASES = {
     'default': {
