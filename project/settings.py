fcd0fa171b3454c2923220ae6c0684ea42e5c831
Inaki Cabo <icaboalo@gmail.com>
2016-03-04 18:20:43 -0600

    temporarely modified permissions

--- project/settings.py
+++ project/settings.py
@@ -97,7 +97,8 @@ REST_FRAMEWORK = {
        'rest_framework.authentication.SessionAuthentication',
    ),
    'DEFAULT_PERMISSION_CLASSES': (
-       'rest_framework.permissions.IsAuthenticated',
+       # 'rest_framework.permissions.IsAuthenticated',
+       'rest_framework.permissions.IsAuthenticatedOrReadOnly',
    ),
    'DEFAULT_FILTER_BACKENDS': (
        'rest_framework.filters.DjangoFilterBackend',
